e71472c2291509578ee4ca3cce2900e9a6cb1df0
lifang <lifang@worktrans.cn>
2022-09-30 18:09:24 +0800

    ArrayListTest提交

---
+++ src/test/java/com/learning/thread/ArrayListTest.java
@@ -0,0 +1,31 @@
+package com.learning.thread;
+
+import com.learning.BaseTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * @author 李芳
+ * @since 2022/9/30
+ */
+public class ArrayListTest extends BaseTest {
+
+    @Test
+    public void test(){
+
+        List<Integer> list = new ArrayList<>();
+
+        IntStream.range(1, 10000).parallel().forEach(num -> list.add(100));
+
+        boolean empty = list.stream().anyMatch(num -> num == null);
+
+        System.out.println("empty: "+ empty);
+    }
+
+}
