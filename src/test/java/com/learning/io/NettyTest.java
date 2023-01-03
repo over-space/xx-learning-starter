c7ab589329a5b4344621002ae7cbde34942f22db
lifang <xxplus@outlook.com>
2023-01-03 21:41:31 +0800

    netty

--- src/test/java/com/learning/io/NettyTest.java
+++ src/test/java/com/learning/io/NettyTest.java
@@ -1,11 +1,15 @@
 package com.learning.io;
 
 import com.learning.BaseTest;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.util.CharsetUtil;
 import org.apache.logging.log4j.LogManager;
@@ -17,6 +21,32 @@ import java.nio.ByteBuffer;
 
 public class NettyTest extends BaseTest {
 
+    @Test
+    void testByteBuffer() {
+
+        // ByteBuffer buffer = ByteBuffer.wrap(new byte[]{'1', '2','3', '4', '5'});// 堆内分配
+        // ByteBuffer buffer = ByteBuffer.allocate(1024);
+        ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
+        printByteBuffer(buffer);
+
+        buffer.put("123456789".getBytes());
+        buffer.put("abc".getBytes());
+        printByteBuffer(buffer);
+
+        byte b = buffer.get();
+        printByteBuffer(buffer);
+        logger.info("buffer#get() : {}", b);
+        printLine();
+
+        buffer.flip();
+        printByteBuffer(buffer);
+
+        buffer.compact();
+        printByteBuffer(buffer);
+
+        buffer.clear();
+        printByteBuffer(buffer);
+    }
 
     @Test
     void testByteBuf01() {
@@ -90,36 +120,85 @@ public class NettyTest extends BaseTest {
         ChannelFuture channelFuture = client.writeAndFlush(byteBuf);
         channelFuture.sync();// 等待数据write成功
 
-        //等待服务端关闭连接
+        // 等待服务端关闭连接
         connect.channel().closeFuture().sync();
     }
 
     @Test
-    void testByteBuffer() {
+    void testNettyServer() throws InterruptedException {
+        NioEventLoopGroup thread = new NioEventLoopGroup(1);
 
-        // ByteBuffer buffer = ByteBuffer.wrap(new byte[]{'1', '2','3', '4', '5'});// 堆内分配
-        // ByteBuffer buffer = ByteBuffer.allocate(1024);
-        ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
-        printByteBuffer(buffer);
+        NioServerSocketChannel server = new NioServerSocketChannel();
 
-        buffer.put("123456789".getBytes());
-        buffer.put("abc".getBytes());
-        printByteBuffer(buffer);
+        thread.register(server);
 
-        byte b = buffer.get();
-        printByteBuffer(buffer);
-        logger.info("buffer#get() : {}", b);
-        printLine();
+        ChannelPipeline pipeline = server.pipeline();
+        // pipeline.addLast(new MyNettyAcceptHandler(thread, new NettyInitHandler()));
+        // pipeline.addLast(new MyNettyAcceptHandler(thread, new ChannelInitialize() {
+        //     @Override
+        //     public void initializeChannel(ChannelHandlerContext ctx) {
+        //         Channel client = ctx.channel();
+        //         client.pipeline().addLast(new MyNettyInHandler());
+        //     }
+        // }));
+        pipeline.addLast(new MyNettyAcceptHandler(thread, new ChannelInitializer<Channel>() {
+            @Override
+            protected void initChannel(Channel client) throws Exception {
+                client.pipeline().addLast(new MyNettyInHandler());
+            }
+        }));
 
-        buffer.flip();
-        printByteBuffer(buffer);
 
-        buffer.compact();
-        printByteBuffer(buffer);
+        ChannelFuture channelFuture = server.bind(new InetSocketAddress("127.0.0.1", 9090));
 
-        buffer.clear();
-        printByteBuffer(buffer);
+        // 等待
+        channelFuture.sync().channel().closeFuture().sync();
     }
+
+    @Test
+    void testNettyClientPro() throws InterruptedException {
+
+        NioEventLoopGroup group = new NioEventLoopGroup(1);
+
+        Bootstrap bootstrap = new Bootstrap();
+        Channel client = bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel client) throws Exception {
+                        client.pipeline().addLast(new MyNettyInHandler());
+                    }
+                }).connect(new InetSocketAddress("127.0.0.1", 9090))
+                .sync()
+                .channel();
+
+        ByteBuf byteBuf = Unpooled.copiedBuffer("hello netty server......\n".getBytes());
+        ChannelFuture channelFuture = client.writeAndFlush(byteBuf);
+        channelFuture.sync();// 等待数据write成功
+
+        client.closeFuture().sync();
+    }
+
+    @Test
+    void testNettyServerPro() throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup(1);
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+
+        Channel server = bootstrap.group(group, group)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel client) throws Exception {
+                        client.pipeline().addLast(new MyNettyInHandler());
+                    }
+                }).bind(9090)
+                .sync()
+                .channel();
+
+        server.closeFuture().sync();
+    }
+
 
     private void printByteBuffer(ByteBuffer buffer) {
         logger.info("position: {}", buffer.position());
@@ -143,6 +222,69 @@ public class NettyTest extends BaseTest {
         printLine();
     }
 
+    class MyNettyAcceptHandler extends ChannelInboundHandlerAdapter {
+
+        private final Logger logger = LogManager.getLogger(MyNettyAcceptHandler.class);
+
+        private final EventLoopGroup thread;
+        private final ChannelHandler handler;
+
+        public MyNettyAcceptHandler(EventLoopGroup thread, ChannelHandler handler) {
+            this.thread = thread;
+            this.handler = handler;
+        }
+
+
+        @Override
+        public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+            logger.info("***** server registered......");
+        }
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            logger.info("****** server active......");
+        }
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            // netty accept之后，获取client
+            SocketChannel client = (SocketChannel) msg;
+
+            logger.info("client : {}", client.remoteAddress());
+
+            // 添加handler,接收client接收server端数据
+            ChannelPipeline pipeline = client.pipeline();
+            pipeline.addLast(handler);
+
+            // 注册到selector
+            thread.register(client);
+        }
+    }
+
+    @ChannelHandler.Sharable
+    class NettyInitHandler extends ChannelInboundHandlerAdapter {
+        @Override
+        public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+            logger.info("***** client registered......");
+            Channel client = ctx.channel();
+            ChannelPipeline pipeline = client.pipeline();
+            pipeline.addLast(new MyNettyInHandler());
+            ctx.pipeline().remove(this);
+        }
+    }
+
+    @ChannelHandler.Sharable
+    abstract class ChannelInitialize extends ChannelInboundHandlerAdapter {
+
+        @Override
+        public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+            initializeChannel(ctx);
+        }
+
+        public abstract void initializeChannel(ChannelHandlerContext ctx);
+    }
+
+    // @ChannelHandler.Sharable 业务代码，如果添加@Sharable注解，业务上需要设计为单例的，不能存在共享资源。业务实现不可控。
     class MyNettyInHandler extends ChannelInboundHandlerAdapter {
 
         private final Logger logger = LogManager.getLogger(MyNettyInHandler.class);
