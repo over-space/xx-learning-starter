96f79b39aa97a18f2f8fe55ac3c6ba9a10171597
lifang <xxplus@outlook.com>
2023-02-21 21:44:06 +0800

    scala

---
+++ src/main/java/com/learning/scala/ClassScalaTest.scala
@@ -0,0 +1,16 @@
+package com.learning.scala
+
+class ClassScalaTest(value:String) {
+  
+  var i = 5;
+  i = 10;
+  
+  val n = 10;
+  
+  println("scala class, a")
+  
+  def map():Unit = {
+    println("scala class, hello scala.....")
+  }
+  println(s"scala class, b ${value}, i=${i}, n=${n + 1}")
+}
---
+++ src/main/java/com/learning/scala/ObjectScalaTest.scala
@@ -0,0 +1,17 @@
+package com.learning.scala
+
+object ObjectScalaTest {
+  
+  println("scala object....1")
+
+  val classScalaTest: ClassScalaTest = new ClassScalaTest("abc")
+  
+  def main(args: Array[String]): Unit = {
+    println("hello world!")
+
+    classScalaTest.map()
+  }
+
+  println("scala object....2")
+  
+}
